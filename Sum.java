034da9263d8c8a6b04203be536633b4e373e813f
sreekanthgiraka <sreekanth.giraka@covalensedigital.net>
2022-05-26 18:36:23 +0530

    commiting sum file

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a = 5;
+			int b = 6;
+			int c =a+b;
+			System.out.println("the output is "+ c);
+	}
+
+}
